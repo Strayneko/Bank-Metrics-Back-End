e9693a6d1dd31fa9b36491f83e579c7d271a9be5
strayneko <bayurendi10@gmail.com>
2023-01-04 16:50:49 +0700

    :zap: add minor change in user login

--- app/Http/Controllers/AuthUserContoller.php
+++ app/Http/Controllers/AuthUserContoller.php
@@ -34,29 +34,33 @@ class AuthUserContoller extends Controller
         ], 201);
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $name = $request->name;
         $email = $request->email;
         $password = $request->password;
 
         $user = User::query()->where('name', $name)->first();
-            if($user == null){
+        if ($user == null) {
             return BaseResponse::error('Name Not Found', 400);
-            }
+        }
 
         $user = User::query()->where('email', $email)->first();
-            if($user == null){
-                return BaseResponse::error('Email Not Found', 400);
-            }
+        if ($user == null) {
+            return BaseResponse::error('Email Not Found', 400);
+        }
 
-        if(!Hash::check($password, $user->password)){
+        if (!Hash::check($password, $user->password)) {
             return BaseResponse::error('Password Failed', 400);
         }
 
+        // add auth token to user data
         $token = $user->createToken('auth_token');
-        return BaseResponse::success([$user,
-                                       'auth' => ['token' => $token->plainTextToken,
-                                                  'token_type' => "Bearer" ]],
-                                      'Login Success', 200);
-     }
+        $user['auth'] = ['tokeen' => 'Bearer ' . $token->plainTextToken];
+        return BaseResponse::success(
+            $user,
+            'Login Success',
+            200
+        );
+    }
 }
