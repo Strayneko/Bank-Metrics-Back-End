9d45d17aacec4253f8fd2e315d66c9e59771210a
Bayu Tresna <bayutresna2000@gmail.com>
2023-01-04 13:29:25 +0700

    add auto hashing for user password

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating (function(User $user){
+            $user->password = Hash::make($user->password);
+        });
+
+        static::updating (function(user $user){
+            if($user->isDirty(['password'])){
+                $user->password = hash::make($user->password);
+            }
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
