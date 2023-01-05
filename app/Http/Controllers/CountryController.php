b91cfdcdb87b9ef6f92566d8a6cf6f263f8aa3d6
strayneko <bayurendi10@gmail.com>
2023-01-05 11:45:56 +0700

    finishing user profile

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Countries;
 use App\Http\Response\BaseResponse;
 use Illuminate\Http\Request;
 
@@ -10,19 +11,8 @@ class CountryController extends Controller
     // show country names
     public function index()
     {
-        // load json file from public folder
-        $json = file_get_contents('json/country_names.json');
-        // convert json to php array
-        $json = json_decode($json);
-        // convert array to laravel collection
-        $json = collect($json);
-        // mapping country data
-        $json = $json->map(function ($country, $idx) {
-            return [
-                'id' => $idx + 1,
-                'name' => $country
-            ];
-        });
-        return BaseResponse::success($json);
+        // get countries data
+        $countries = countries::getCountries();
+        return BaseResponse::success($countries);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthUserContoller;
+use App\Http\Controllers\CountryController;
 
 /*
 |--------------------------------------------------------------------------
