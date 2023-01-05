c18e2b4dcb1f8e5410ecc401bf39c1bbab919e39
Ciman Code <firmanabdul278@gmail.com>
2023-01-05 15:04:31 +0700

    (+) solve api login

--- app/Http/Controllers/AuthUserContoller.php
+++ app/Http/Controllers/AuthUserContoller.php
@@ -36,14 +36,8 @@ class AuthUserContoller extends Controller
 
     public function login(Request $request)
     {
-        $name = $request->name;
         $email = $request->email;
         $password = $request->password;
-
-        $user = User::query()->where('name', $name)->first();
-        if ($user == null) {
-            return BaseResponse::error('Name Not Found', 400);
-        }
 
         $user = User::query()->where('email', $email)->first();
         if ($user == null) {
