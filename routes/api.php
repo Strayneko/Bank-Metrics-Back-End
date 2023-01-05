a80e285f479605e72c7274fb966217ada256001d
strayneko <bayurendi10@gmail.com>
2023-01-05 11:15:30 +0700

    fix minor bug

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AuthRegisterController;
 use App\Http\Controllers\BankController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthUserContoller;
 
 /*
 |--------------------------------------------------------------------------
