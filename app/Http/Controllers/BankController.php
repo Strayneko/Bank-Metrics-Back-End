ad94d891559523f718374fe6048040676c849469
bayutresna <bayutresna2000@gmail.com>
2023-01-16 15:31:19 +0700

    fix update and add soft delete for bank

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -1,11 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Http\Response\BaseResponse;
 use App\Models\Bank;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class BankController extends Controller
 {
@@ -43,27 +41,27 @@ class BankController extends Controller
         return BaseResponse::success($bank, 'Data was successfully created');
     }
 
-    function update(Request $request)
+    function update(Request $request, $id)
     {
-        // get authenticated user
-        $user = Auth::user();
-        $bank = Bank::query()->where('id', $user->id)->first();
+
+        $bank = Bank::query()->where('id', $id)->first();
         if (!$bank) BaseResponse::error('Data was not found', 404);
         try {
             $validated = $request->validate([
-                'name' => ['required', 'max:50', 'min:3'],
-                'loaning_percentage' => ['required', 'numeric', 'min:1', '100'],
-                'max_age' => ['required', 'min:1', 'max:150',  'numeric'],
-                'min_age' => ['required', 'min:1', 'max:150'],
-                'marital_status' => ['required', 'numeric', 'digits_between:0,1'],
-                'nationality' => ['required', 'numeric', 'min:1'],
-                'employment' => ['required', 'numeric', 'digits_between:0,1'],
+                'name' => [ 'max:50', 'min:3'],
+                'loaning_percentage' => [ 'numeric', 'min:1', '100'],
+                'max_age' => [ 'min:1', 'max:150',  'numeric'],
+                'min_age' => [ 'min:1', 'max:150'],
+                'marital_status' => [ 'numeric', 'digits_between:0,1'],
+                'nationality' => [ 'numeric', 'min:1'],
+                'employment' => [ 'numeric', 'digits_between:0,1'],
             ]);
         } catch (\Illuminate\Validation\ValidationException $validate) {
             return BaseResponse::error($validate->validator->errors()->all());
         }
 
         $bank->fill($validated);
+        $bank->save();
         return BaseResponse::success($bank, 'Data was successfully updated');
     }
 
--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
     protected $guarded = ['id'];
     protected $hidden = [
         'created_at',
