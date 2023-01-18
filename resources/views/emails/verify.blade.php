487677a71beaa861773ac5a5fd94a5d08c4d609c
Ciman Code <firmanabdul278@gmail.com>
2023-01-18 15:40:36 +0700

    (+) api verfy email

---
+++ resources/views/emails/verify.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+  <title>Metrics Reset Password</title>
+  <meta name="description" content="Metrics Reset Password.">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+  <style type="text/css">
+    * {
+      font-family: 'Poppins', sans-serif;
+    }
+
+    a:hover {
+      text-decoration: underline !important;
+    }
+  </style>
+</head>
+
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+  <!--100% body table-->
+  <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+    style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+    <tr>
+      <td>
+        <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+          align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td style="height:80px;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align:center;">
+              <a href="https://metrics-fe.fly.dev" title="Metrics" target="_blank">
+                <img width="200" src="https://i.ibb.co/Fq4J9xS/Logo.png" title="Metrics" alt="Metrics">
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td style="height:20px;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td>
+              <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                style="max-width:670px;background:#fff; border-radius:12px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                <tr>
+                  <td style="height:40px;">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td style="padding:0 35px;">
+                    <h1
+                      style="color:#989898; font-weight:700; margin:0;font-size:32px;font-family:'Poppins',sans-serif;">
+                      You have requested to reset your password</h1>
+                    <span
+                      style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                    <p style="color:#989898; font-size:15px;line-height:24px; margin:0;">
+                      We cannot simply send you your old password. A unique link to reset your
+                      password has been generated for you. To reset your password, click the
+                      following link and follow the instructions.
+                    </p>
+                    <a href="http://127.0.0.1:4000/verifyEmail/{{ $confirmation_code }}"
+                      style="background:#FCC997;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Verify Your Email</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="height:40px;">&nbsp;</td>
+                </tr>
+              </table>
+            </td>
+          <tr>
+            <td style="height:20px;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align:center;">
+              <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">
+                &copy; <strong>metrics-fe.fly.dev</strong></p>
+            </td>
+          </tr>
+          <tr>
+            <td style="height:80px;">&nbsp;</td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+  <!--/100% body table-->
+</body>
+
+</html>
