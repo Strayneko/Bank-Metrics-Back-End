333885ff61ad68628fe438f9a8a7847535a677ff
bayutresna <bayutresna2000@gmail.com>
2023-01-19 13:13:39 +0700

    fix api route

--- routes/api.php
+++ routes/api.php
@@ -58,7 +58,7 @@ Route::prefix('admin')
     });
 
 Route::prefix('user')
-    ->middleware(['auth:sanctum', 'hasApiKey', 'verified'])
+    ->middleware(['auth:sanctum', 'hasApiKey'])
     ->group(function () {
         Route::get('/', [UserController::class, 'index']);
         Route::get('/me', [UserController::class, 'show']);
@@ -78,7 +78,7 @@ Route::prefix('bank')->middleware(["auth:sanctum", 'hasApiKey'])->group(function
 // loan group prefix
 Route::prefix('loan')
     ->controller(LoanController::class)
-    ->middleware(['auth:sanctum', 'hasApiKey', 'verified'])
+    ->middleware(['auth:sanctum', 'hasApiKey'])
     ->group(function () {
         Route::post('/get_loan', 'loan');
         Route::get('/list/', 'list_loan');
