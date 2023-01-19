59953ca3dab991d6868cd9fc5aadf4b9ad2b0f61
bayutresna <bayutresna2000@gmail.com>
2023-01-19 11:42:49 +0700

    fix verify email

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -51,15 +51,13 @@
                   <td style="padding:0 35px;">
                     <h1
                       style="color:#989898; font-weight:700; margin:0;font-size:32px;font-family:'Poppins',sans-serif;">
-                      You have requested to reset your password</h1>
+                      Verify Your Email</h1>
                     <span
                       style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
                     <p style="color:#989898; font-size:15px;line-height:24px; margin:0;">
-                      We cannot simply send you your old password. A unique link to reset your
-                      password has been generated for you. To reset your password, click the
-                      following link and follow the instructions.
+                      You must verify your email before using this application. please click on the button bellow.
                     </p>
-                    <a href="http://127.0.0.1:4000/verifyEmail/{{ $confirmation_code }}"
+                    <a href="{{ env('APP_URL') }}/verifyEmail/{{ $confirmation_code }}"
                       style="background:#FCC997;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Verify Your Email</a>
                   </td>
                 </tr>
